e16d30665b097b70010f801f8b41dc094d756ff5
vossiepkv <vossiepkv@gmail.com>
2024-09-10 23:13:52 +1000

    fixing bugs for vercel

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: './', // Ensure the base path is relative
-  build: {
-    outDir: 'dist', // Make sure output directory is correctly configured
-  }
-})
